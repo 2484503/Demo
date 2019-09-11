fb7cd0a3d1c3a5d4e9041da39a2ddbe95f44998f
lijiannan <2484503@qq.com>
2019-09-11 15:38:38 +0800

    add package-info

---
+++ src/main/java/com/test/utils/serializer/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author lijn
+ * @version 1.0
+ * @date 2019/9/11 15:14
+ */
+package com.test.utils.serializer;
+
+/**
+ * 此序列化工具相比fastjson对LocalDataTime有更好的格式维护，在反序列化的时候，会更稳定，在此表示龙神牛逼
+ * */
